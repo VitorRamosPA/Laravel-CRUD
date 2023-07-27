bb3b889396e335c4a0a2095f150f8dfa9cb41c8d
Vitor Ramos <vitorramosti@gmail.com>
2023-07-27 12:29:48 -0300

    create routes to cadastro page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,16 +4,21 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                <div class="shrink-0 flex items-center dark:text-gray-100">
+                    <a href="{{ route('dashboard') }}" >
+                        &#x2615 VRamos
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Cadastro') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('lista')" :active="request()->routeIs('lista')">
+                        {{ __('Lista') }}
                     </x-nav-link>
                 </div>
             </div>
